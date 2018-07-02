31638aaf809596014e1164ad718dc81ddcdf8a11
cajimon04 <cajimon04@gmail.com>
2018-07-03 01:27:34 +0200

    Adivina el numero dos jugadores

---
+++ adivina_el_numero_dos_jugadores.py
@@ -0,0 +1,29 @@
+print("Bienvenido a Adivina el numero")
+
+numero_a_adivinar = input("Escribe el numero que quieres que tu compañero adivine, pidele que no mire: ")
+
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+print("No mires arriba")
+
+intento_usuario = input("Trata de adivinarlo(Dale pistas): ")
+
+while intento_usuario != numero_a_adivinar:
+    intento_usuario = input("Trata de adivinarlo(Dale pistas): ")
+
+print("LO CONSEGUISTE ERES UN MAKINA")
