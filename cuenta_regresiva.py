8b227033c534d18609334de04dd8d96a66da00ad
cajimon04 <cajimon04@gmail.com>
2018-07-02 16:22:23 +0200

    Primer intento cuenta regresiva

---
+++ cuenta_regresiva.py
@@ -0,0 +1,12 @@
+numero_inicial = 10
+print(numero_inicial)
+print("Es par")
+
+while numero_inicial > 0:
+    numero_inicial -= 1
+    print (numero_inicial)
+    if numero_inicial % 2 == 0:
+        print("Es par")
+    else:
+        print("Es impar")
+print("Ya ta")
